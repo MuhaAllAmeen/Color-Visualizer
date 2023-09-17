1a287a98ad8113c9470e86cd7350e983c7d4831c
Muhammed Al Ameen Rahman <muhaallameen@gmail.com>
2023-09-17 19:18:04 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="color-input">
+      <input type='number' onChange={changeColor} name='color1' min={0} max={255} defaultValue={1}></input>
+      <input type='number' onChange={changeColor} name='color2' min={0} max={255} defaultValue={1}></input>
+      <input type='number' onChange={changeColor} name='color3' min={0} max={255} defaultValue={1}></input>
     </div>
   );
 }
+
+function changeColor(){
+  const body = document.getElementsByTagName('body')
+  const color1 = document.getElementsByName('color1')[0].value
+  const color2 = document.getElementsByName('color2')[0].value
+  const color3 = document.getElementsByName('color3')[0].value
+  console.log(color1.value)
+  document.body.style.backgroundColor = `rgb(${color1},${color2},${color3})`
+}
+
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
 
 export default App;
